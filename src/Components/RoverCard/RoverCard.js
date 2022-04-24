193ba669b7df2947ea98147899d121b9e12a5677
Sam Mistele <smmstl0@gmail.com>
2022-04-24 08:29:50 -0600

    Add delete functionality

--- src/Components/RoverCard/RoverCard.js
+++ src/Components/RoverCard/RoverCard.js
@@ -5,7 +5,7 @@ const RoverCard = ({ camera, img_src, rover, id, deleteImage }) => {
 
   return (
     <div className='card' onClick={() => {deleteImage(id)}}>
-      <img className ='rover-card' src={img_src} height='240px' width='240px'/>
+      <img className='rover-card' src={img_src} height='240px' width='240px'/>
       <div className='hover-text'>
         <p className='camera'>{camera.full_name}</p>
         <p className='remove-img'>click to remove</p>
